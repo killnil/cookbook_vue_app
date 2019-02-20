9648ccc0d95eb47bb0fcf87c7898b21537cac88e
JackMarx <seriousfools@gmail.com>
2019-02-20 09:58:13 -0600

    updates

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,28 +1,5 @@
 <template>
   <div class="home">
-    <h1>New Recipe</h1>
-    <div>
-      <div>
-        Title: <input v-model="newRecipeTitle">
-      </div>
-      <div>
-        Chef: <input v-model="newRecipeChef">
-      </div>
-      <div>
-        Prep Time: <input v-model="newRecipePrepTime">
-      </div>
-      <div>
-        Ingredients: <input v-model="newRecipeIngredients">
-      </div>
-      <div>
-        Directions: <input v-model="newRecipeDirections">
-      </div>
-      <div>
-        Image URL: <input v-model="newRecipeImageUrl">
-      </div>
-      <button v-on:click="createRecipe()">Create</button>
-    </div>
-
     <h1>All Recipes</h1>
     <div v-for="recipe in recipes">
       <h2>{{ recipe.title }}</h2>
@@ -78,12 +55,6 @@ export default {
   data: function() {
     return {
       recipes: [],
-      newRecipeTitle: "",
-      newRecipeChef: "",
-      newRecipePrepTime: "",
-      newRecipeIngredients: "",
-      newRecipeDirections: "",
-      newRecipeImageUrl: "",
       currentRecipe: {}
     };
   },
@@ -100,22 +71,6 @@ export default {
       } else {
         this.currentRecipe = inputRecipe;
       }
-    },
-    createRecipe: function() {
-      console.log("Create the Recipe...");
-      var params = {
-                    title: this.newRecipeTitle,
-                    chef: this.newRecipeChef,
-                    prep_time: this.newRecipePrepTime,
-                    ingredients: this.newRecipeIngredients,
-                    directions: this.newRecipeDirections,
-                    image_url: this.newRecipeImageUrl
-                    };
-      axios.post("/api/recipes", params)
-        .then(response => {
-          console.log("Success", response.data);
-          this.recipes.push(response.data);
-        });
     },
     updateRecipe: function(inputRecipe) {
       var params = {
