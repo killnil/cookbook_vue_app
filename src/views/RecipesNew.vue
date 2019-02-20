0610e0f8acb62d019de7860348b159f2c90ae792
JackMarx <seriousfools@gmail.com>
2019-02-20 09:42:38 -0600

    recipes new

---
+++ src/views/RecipesNew.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="recipes-new">
+    <h1>New Recipe</h1>
+    <div>
+      <div>
+        Title: <input v-model="newRecipeTitle">
+      </div>
+      <div>
+        Chef: <input v-model="newRecipeChef">
+      </div>
+      <div>
+        Prep Time: <input v-model="newRecipePrepTime">
+      </div>
+      <div>
+        Ingredients: <input v-model="newRecipeIngredients">
+      </div>
+      <div>
+        Directions: <input v-model="newRecipeDirections">
+      </div>
+      <div>
+        Image URL: <input v-model="newRecipeImageUrl">
+      </div>
+      <button v-on:click="createRecipe()">Create</button>
+    </div>
+  </div>
+</template>
+
+<style>
+</style>
+
+<script>
+var axios = require('axios');
+
+export default {
+  data: function() {
+    return {
+      newRecipeTitle: "",
+      newRecipeChef: "",
+      newRecipePrepTime: "",
+      newRecipeIngredients: "",
+      newRecipeDirections: "",
+      newRecipeImageUrl: ""
+    };
+  },
+  created: function() {},
+  methods: {
+    createRecipe: function() {
+      console.log("Create the Recipe...");
+      var params = {
+                    title: this.newRecipeTitle,
+                    chef: this.newRecipeChef,
+                    prep_time: this.newRecipePrepTime,
+                    ingredients: this.newRecipeIngredients,
+                    directions: this.newRecipeDirections,
+                    image_url: this.newRecipeImageUrl
+                    };
+      axios.post("/api/recipes", params)
+        .then(response => {
+          console.log("Success", response.data);
+          this.$router.push("/");
+        });
+    }
+  }
+};
+</script>
